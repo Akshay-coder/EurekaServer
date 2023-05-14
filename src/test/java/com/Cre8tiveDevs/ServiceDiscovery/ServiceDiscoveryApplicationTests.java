10fed2b47183b08ddbc8053786eb5e25b3c0cd2b
Akshay kamble <kambleakshay071@gmail.com>
2023-05-14 13:25:39 +0530

    initial commit

---
+++ src/test/java/com/Cre8tiveDevs/ServiceDiscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cre8tiveDevs.ServiceDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
